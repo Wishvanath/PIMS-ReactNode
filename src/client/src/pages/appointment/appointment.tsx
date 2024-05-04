f96668a321df0e52548800fcf92adbb90b7985dd
Wishvanath Sah <anandwishvanath@gmail.com>
2024-05-04 14:30:27 +0530

    Updated login page

--- src/client/src/pages/appointment/appointment.tsx
+++ src/client/src/pages/appointment/appointment.tsx
@@ -1,28 +1,53 @@
-import React, { useEffect } from 'react';
-import { Button } from 'antd';
+import React, { useEffect, useState } from 'react';
+import { Button, Spin } from 'antd';
 import { useDispatch, useSelector } from 'react-redux';
 
-import { fetchAppointmentDetailsById, fetchAllAppointmentDetails } from './appointment-thunk';
+import {
+  fetchAppointmentDetailsById,
+  fetchAllAppointmentDetails,
+} from './appointment-thunk';
 import { hasSucceded } from '../../utils/async-status';
 import { IRootState } from '../../app/store';
-import { selectAppointmentDetailsById, selectAllAppointmentDetails } from './appointment-selector';
+import {
+  selectAppointmentDetailsById,
+  selectAllAppointmentDetails,
+  selectAllAppointmentDetailsAsyncStatus,
+  selectAppointmentDetailsByIdAsyncStatus,
+} from './appointment-selector';
+
+const contentStyle: React.CSSProperties = {
+  padding: 50,
+  background: 'rgba(0, 0, 0, 0.05)',
+  borderRadius: 4,
+};
+const content = <div style={contentStyle} />;
 
 const Appointment = () => {
   const dispatch = useDispatch();
   const appointmentById = useSelector(selectAppointmentDetailsById);
   const allAppointmentDetails = useSelector(selectAllAppointmentDetails);
+  const allAppointmentDetailsAsyncStatus = useSelector(
+    selectAllAppointmentDetailsAsyncStatus
+  );
+  const appointmentDetailsByIdAsyncStatus = useSelector(
+    selectAppointmentDetailsByIdAsyncStatus
+  );
+  // const [demoLoading, setDemoLoading] = useState(false);
+
+  // useEffect(() => {
+  //   setTimeout(() => {
+  //     setDemoLoading(true)
+  //   }, 5000)
+  // }, [])
 
   const bodyPyload: any = {
     limit: 10,
     offset: 0,
-    keyword: "Wish",
+    keyword: 'Wish',
     filters: {
-        assignedDoctor: [
-            1,
-            2
-        ]
-    }
-  }
+      assignedDoctor: [1, 2],
+    },
+  };
 
   console.log(appointmentById);
 
@@ -31,24 +56,37 @@ const Appointment = () => {
   }, []);
 
   useEffect(() => {
-    dispatch(fetchAllAppointmentDetails(
-      bodyPyload
-    ));
-    
+    dispatch(fetchAllAppointmentDetails(bodyPyload));
   }, []);
 
-
-
+  const loading =
+    hasSucceded(allAppointmentDetailsAsyncStatus) &&
+    hasSucceded(appointmentDetailsByIdAsyncStatus);
 
   return (
     <>
+      {loading ? (
+        <>
+          <h1>Single Appointment</h1>
+          {JSON.stringify(appointmentById)}
+
+          <h1>All Appointment details data</h1>
+          {JSON.stringify(allAppointmentDetails)}
+        </>
+      ) : (
+        <>
+          <Spin tip="Loading" size="large">
+            {content}
+          </Spin>
+        </>
+      )}
+      {/* <Spin tip="Loading">
       <h1>Single Appointment</h1>
-      {JSON.stringify(appointmentById)}
+          {JSON.stringify(appointmentById)}
 
-      <h1>All Appointment details data</h1>
-      {JSON.stringify(allAppointmentDetails)}
-
-
+          <h1>All Appointment details data</h1>
+          {JSON.stringify(allAppointmentDetails)}
+      </Spin> */}
     </>
   );
 };
--- src/client/src/pages/login/login.tsx
+++ src/client/src/pages/login/login.tsx
@@ -1,8 +1,82 @@
+// const Login = () => {
+//   return (
+//     <>
+//       <h1>This is Login page</h1>
+//     </>
+//   );
+// };
+// export default Login;
+
+import React, { useState } from 'react';
+import { Form, Input, Button, Checkbox, Typography, message } from 'antd';
+import axios from 'axios';
+// import 'antd/dist/antd.css';
+// import './App.css';
+
+const { Title } = Typography;
+
 const Login = () => {
+  const [loading, setLoading] = useState(false);
+
+  const onFinish = async (values:any) => {
+    setLoading(true);
+    try {
+      // Simulate API request
+      // console.log("Form Values:=====>", values.username);
+      // const response = await axios.post('https://your-api-endpoint.com/login', values);
+      // console.log('Login Successful:', response.data);
+      // Redirect or do something after successful login
+      if(values.username === 'wishvanath' && values.password === 'admin'){
+        message.success('Login Successful!');
+        window.location.href ="/dashboard"
+      }else{
+        message.error('Invalid credentials!');
+
+      }
+    } catch (error) {
+      console.error('Login Failed:', error);
+      message.error('Login Failed!');
+    }
+    setLoading(false);
+  };
+
   return (
-    <>
-      <h1>This is Login page</h1>
-    </>
+    <div className="login-container">
+      <Title level={2}>Login</Title>
+      <Form
+        name="login-form"
+        initialValues={{ remember: true }}
+        onFinish={onFinish}
+        layout="vertical"
+      >
+        <Form.Item
+          label="Username"
+          name="username"
+          rules={[{ required: true, message: 'Please input your username!' }]}
+        >
+          <Input />
+        </Form.Item>
+
+        <Form.Item
+          label="Password"
+          name="password"
+          rules={[{ required: true, message: 'Please input your password!' }]}
+        >
+          <Input.Password />
+        </Form.Item>
+
+        <Form.Item name="remember" valuePropName="checked">
+          <Checkbox>Remember me</Checkbox>
+        </Form.Item>
+
+        <Form.Item>
+          <Button type="primary" htmlType="submit" loading={loading}>
+            Login
+          </Button>
+        </Form.Item>
+      </Form>
+    </div>
   );
 };
+
 export default Login;
